059dffacf03d76a0aaa318278233f325b64f8f3b
Pierre Segalen <pierre.segalen@gmail.com>
2020-11-06 11:48:20 +0100

    Updated projetMovies

--- projetMovies/index.js
+++ projetMovies/index.js
@@ -1,20 +1,48 @@
+function creerCase(media) {
+  var result = document.createElement("a");
+  result.href = "media.html?id=" + media.id + "&type=" + media.media_type;
+  result.className = "media";
+  var img = document.createElement("img");
+  img.src = urlDuPoster(media);
+  img.className = "poster";
+  result.appendChild(img);
+  var infos = document.createElement("div");
+  infos.className = "infos";
+  result.appendChild(infos);
+  var titre = document.createElement("span");
+  titre.innerText = media.media_type == "movie" ? media.title : media.name;
+  titre.className = "titre";
+  infos.appendChild(titre);
+  var type = document.createElement("span");
+  type.innerText = media.media_type == "movie" ? "Film" : "Série";
+  type.className = "type";
+  infos.appendChild(type);
+  var annee = document.createElement("span");
+  annee.innerText =
+    media.media_type == "movie"
+      ? media.release_date
+        ? media.release_date.split("-")[0]
+        : "Année inconnue"
+      : media.first_air_date
+      ? media.first_air_date.split("-")[0]
+      : "Année inconnue";
+  annee.className = "annee";
+  infos.appendChild(annee);
+  return result;
+}
+
 function traiterResultats(resultats) {
   viderListe();
   // A faire : mettre en forme la liste de résultats
   if (resultats.total_results > 0) {
     id("nbResultats").innerText = resultats.total_results + " résultats !";
-    // Attention au champ "media_type", il faut filtrer sur "movie" ou "tv" !
-    // Les lignes suivantes afficheront une image cassée si le premier résultat est de type "person"
-    var img = document.createElement("img");
-    img.src = urlDuPoster(resultats.results[0]);
-    var lien = document.createElement("a");
-    lien.href =
-      "media.html?id=" +
-      resultats.results[0].id +
-      "&type=" +
-      resultats.results[0].media_type;
-    lien.appendChild(img);
-    id("listeResultats").appendChild(lien);
+
+    for (let i = 0; i < resultats.results.length; i++) {
+      const element = resultats.results[i];
+      if (element.media_type == "movie" || element.media_type == "tv") {
+        id("listeResultats").appendChild(creerCase(element));
+      }
+    }
   } else {
     id("nbResultats").innerText = "Pas de résultat !";
   }
@@ -27,12 +55,52 @@ function rechercher() {
       "https://api.themoviedb.org/3/search/multi?api_key=97719463bea4bd4b5902c1a735c0556a&language=fr-FR&query=" +
         motsRecherches
     )
-    .then(result => traiterResultats(result.data))
+    .then(result => {
+      localStorage.setItem("medias", JSON.stringify(result.data));
+      var recherches = localStorage.getItem("recherches")
+        ? JSON.parse(localStorage.getItem("recherches"))
+        : [];
+      recherches.push(motsRecherches);
+      localStorage.setItem("recherches", JSON.stringify(recherches));
+      traiterResultats(result.data);
+    })
     .catch(error => alert(error.message));
 }
 
+function verifierCache() {
+  var resultatsPrecedents = JSON.parse(localStorage.getItem("medias"));
+  var recherches = JSON.parse(localStorage.getItem("recherches"));
+  if (resultatsPrecedents) {
+    traiterResultats(resultatsPrecedents);
+    if (recherches && recherches.length > 0) {
+      id("motsRecherches").value = recherches[recherches.length - 1];
+      // On inverse le tableau et on récupère les éléments 1 à 6
+      var histo = recherches.reverse().slice(1, 6);
+      for (var i = 0; i < histo.length; i++) {
+        var mot = histo[i];
+        var btn = document.createElement("button");
+        btn.innerText = mot;
+        // Le truc moche ci-dessous s'appelle une closure et dans ce cas précis, on n'a pas le choix
+        btn.addEventListener(
+          "click",
+          (function(mot) {
+            return function() {
+              id("motsRecherches").value = mot;
+              rechercher();
+            };
+          })(mot)
+        );
+        btn.style.marginRight = "16px";
+        id("historique").appendChild(btn);
+      }
+    }
+  }
+}
+
 function urlDuPoster(resultat) {
-  return "https://image.tmdb.org/t/p/w780" + resultat.poster_path;
+  return resultat.poster_path
+    ? "https://image.tmdb.org/t/p/w342" + resultat.poster_path
+    : "https://lh3.googleusercontent.com/proxy/dZsw-A4QmKEUGDv_rj_y7HtwTaTSM1HjhgZHogrb-HQAjF6VYY8YhLVRjZvFXvh3zvNRSiSDU0edc-HQYVzQ0LKSqfnD9eYzbHB12tow0O7IFk8w3n9YFSskLFTYhfaXmKRM";
 }
 
 function viderListe() {
--- projetMovies/media.js
+++ projetMovies/media.js
@@ -1,15 +1,73 @@
 function traiterFilm(film) {
   id("titre").innerText = film.title;
+  ajouterInfosCommunes(film);
+  var infos = id("infos");
+  var budget = document.createElement("div");
+  budget.innerText = `Budget : ${
+    film.budget ? formaterMontant(film.budget) : "?"
+  } / Revenue : ${film.revenue ? formaterMontant(film.revenue) : "?"}`;
+  infos.appendChild(budget);
+  var duree = document.createElement("div");
+  duree.innerText = "Durée : " + formaterDuree(film.runtime);
+  infos.appendChild(duree);
+}
+
+function formaterDuree(duree) {
+  if (duree) {
+    var nbHeures = Math.floor(duree / 60);
+    var nbMinutes = duree % 60;
+    return nbHeures + " h " + nbMinutes;
+  } else {
+    return "inconnue";
+  }
+}
+
+function formaterMontant(montant) {
+  var montantEnEuros = Math.round(montant / 1.1);
+  if (montantEnEuros > 1000000)
+    return (montantEnEuros / 1000000).toFixed(1) + " M€";
+  else if (montantEnEuros > 1000)
+    return (montantEnEuros / 1000).toFixed(1) + " K€";
+  else return montantEnEuros + " €";
 }
 
 function traiterSerie(serie) {
   id("titre").innerText = serie.name;
+  ajouterInfosCommunes(serie);
+  var infos = id("infos");
+  var episodes = document.createElement("div");
+  episodes.innerText = `Nombre de saisons : ${serie.seasons.length}`;
+  infos.appendChild(episodes);
+  for (let i = 0; i < serie.seasons.length; i++) {
+    const element = serie.seasons[i];
+    var saison = document.createElement("div");
+    saison.innerText = `Saison ${i + 1} : ${element.episode_count} épisodes`;
+    infos.appendChild(saison);
+  }
+  for (let i = 0; i < serie.networks.length; i++) {
+    const element = serie.networks[i];
+    if (element.logo_path) {
+      var plateforme = document.createElement("img");
+      plateforme.src = urlPlateforme(element.logo_path);
+      infos.appendChild(plateforme);
+    }
+  }
+}
+
+function ajouterInfosCommunes(media) {
+  id("backdrop").style.background = `url(${urlDuBackdrop(
+    media
+  )}) center center`;
+
+  id("genre").innerText =
+    media.genres && media.genres.length > 0 ? media.genres[0].name : "";
+  id("votes").innerText = `${media.vote_average} (${media.vote_count})`;
 }
 
 function chargerMedia() {
   // Il faut récupérer cette info de l'URL, si l'id est celui d'une série, ça ne fonctionnera pas
-  var mediaType = "movie";
-  var id = recupererParametre();
+  var mediaType = recupererParametre("type");
+  var id = recupererParametre("id");
   if (id.length == 0) {
     alert("Il y a un souci dans l'URL !");
   } else {
@@ -29,12 +87,22 @@ function chargerMedia() {
   }
 }
 
-function recupererParametre() {
+function urlPlateforme(logo) {
+  return "https://image.tmdb.org/t/p/w154" + logo;
+}
+
+function urlDuBackdrop(media) {
+  return media.backdrop_path
+    ? "https://image.tmdb.org/t/p/w1280" + media.backdrop_path
+    : "https://lh3.googleusercontent.com/proxy/dZsw-A4QmKEUGDv_rj_y7HtwTaTSM1HjhgZHogrb-HQAjF6VYY8YhLVRjZvFXvh3zvNRSiSDU0edc-HQYVzQ0LKSqfnD9eYzbHB12tow0O7IFk8w3n9YFSskLFTYhfaXmKRM";
+}
+
+function recupererParametre(nomParam) {
   // Spoiler : en modifiant légèrement cette fonction, elle pourrait aussi servir à récupérer le type de média
   var keyValues = document.location.search.replace("?", "").split("&");
   for (let i = 0; i < keyValues.length; i++) {
     const keyValue = keyValues[i].split("=");
-    if (keyValue[0] == "id") {
+    if (keyValue[0] == nomParam) {
       return keyValue[1];
     }
   }
